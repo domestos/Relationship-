3361b0ad9521a4fa6700e39e8f4fa3bb8b0bb160
Domestos <elBarto2006@ukr.net>
2016-03-02 00:38:22 +0200

    Relationship between Activity and AsyncTask

---
+++ app/src/main/java/com/example/vpelenskyi/saveactivity/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.vpelenskyi.saveactivity;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends FragmentActivity implements View.OnClickListener {
+    private Button btnStart;
+    private Button btnStop;
+    public TextView tvUpdate;
+    private ProgressBar progressBar;
+    private MainFragment mainFragment;
+    private String TAG_FRAGMENT = "my_fragment";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvUpdate = (TextView) findViewById(R.id.tvUpdate);
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnStop = (Button) findViewById(R.id.btnStop);
+        progressBar = (ProgressBar) findViewById(R.id.progress);
+        btnStop.setOnClickListener(this);
+        btnStart.setOnClickListener(this);
+
+        mainFragment = getMainFragment();
+        //Передаємо об"єкту MainFragment актівіті
+        mainFragment.link(this);
+        // визначаємо в якому стані AsyncTask
+        // mainFragment.getIsWork() - повертає true якщо виконується процес, false - якщо він в стані спокою
+        showProgress(mainFragment.getIsWork());
+    }
+
+
+    public MainFragment getMainFragment() {
+        //перевіряємо чи існує фрагмент і присвоюємо результат перевірки змінній
+        mainFragment = (MainFragment) getFragmentManager().findFragmentByTag(TAG_FRAGMENT);
+        //якщо перевірка верне null, то означає,
+        // що фрагмент раніше не був створений і його нобхідно створити
+        if (mainFragment == null) {
+            mainFragment = new MainFragment();
+            //записуємо створений екземпляр MainFragment у FragmentManager
+            getFragmentManager().beginTransaction().add(mainFragment, TAG_FRAGMENT).commit();
+        }
+        return mainFragment;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnStart:
+                mainFragment.start();
+                break;
+            case R.id.btnStop:
+                mainFragment.stop();
+                break;
+        }
+    }
+
+    // показує стан AsyncTasks
+    public void showProgress(boolean show) {
+        btnStart.setEnabled(!show);
+        btnStop.setEnabled(show);
+        progressBar.setVisibility(show ? View.VISIBLE : View.GONE);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // завершує роботу AsyncTask якщо нажата кнопка Back
+        if (isFinishing()) {
+            mainFragment.stop();
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/vpelenskyi/saveactivity/MainFragment.java
@@ -0,0 +1,101 @@
+package com.example.vpelenskyi.saveactivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * Created by v.pelenskyi on 01.03.2016.
+ */
+public class MainFragment extends android.app.Fragment {
+    private boolean isWork = false; //інформує актівіті про стан AsyncTask
+    private FragmentTask fragmentTask;
+    private MainActivity activity;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);//заборона перестворювати фрагмент при повороті екрану
+    }
+
+    /**
+     * Даний метод викликається в методі onCreate() класу MainActivity,
+     * тому ми завжди будемо мати актуальний activity, навіть при повороті екрану.
+     */
+    public MainActivity link(MainActivity activity) {
+        return this.activity = activity;
+    }
+
+    /**
+     * цей метод демонструє звязок між класом MainFragment та MainActivity
+     * але замість ньоги ми будемо використовувати метод link(...).
+     */
+    public MainActivity getMainActvity() {
+        activity = (MainActivity) this.getActivity();
+        return activity;
+    }
+
+    public void start() {
+
+        fragmentTask = new FragmentTask();
+        fragmentTask.execute();
+    }
+
+    public void stop() {
+        if (fragmentTask != null) {
+            fragmentTask.cancel(true);
+        }
+    }
+
+    public boolean getIsWork() {
+        return isWork;
+    }
+
+    /**
+     *   Довгограючий клас :)
+     */
+    public class FragmentTask extends AsyncTask<Void, Integer, Boolean> {
+        @Override
+        protected void onPreExecute() {
+            isWork = true;
+            activity.showProgress(isWork);
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            try {
+                for (int i = 1000; i >= 0; i--) {
+                    publishProgress(i);
+                    Thread.sleep(10);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            // activity = getMainActvity();
+            activity.tvUpdate.setText(values[0].toString());
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            //   activity = getMainActvity();
+            activity.showProgress(false);
+            isWork = false;
+            fragmentTask = null;
+        }
+
+        @Override
+        protected void onCancelled() {
+            Log.i("log", "cancelled");
+            isWork = false;
+            activity.showProgress(isWork);
+            super.onCancelled();
+        }
+    }
+
+}
